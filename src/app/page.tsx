201d2aaf54d5eb2301677659ef0bba35405f329d
MatiasFabeiro <matiasfabeiro@gmail.com>
2024-05-24 18:30:19 -0300

    Updates Added more options to type structure of movies, changed the style of the searchbar and improved the style of the home movies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,31 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center lg:p-24 md:flex-row md:flex-wrap md:gap-10 lg:gap-20">
-        {popularMoviesWeek?.map((data) => (
-          <Image
-            src={`${imageURL}${data.poster_path}`}
-            alt={`${data.title} Image`}
-            key={`${data.title}`}
-            width={220}
-            height={330}
-            className="mb-8"
-          >
-          </Image>
-        ))}
+        {popularMoviesWeek?.map((data) => {
+          const title = data.title || data.name || data.original_title || data.original_name;
+
+          return(
+            <div
+              key={title}
+              className="flex flex-col justify-center items-center"
+            >
+              <p className="w-11/12 max-w-[200px] h-[80px] flex items-center justify-center text-xl text-center my-4"> 
+                {data.title ? data.title : 
+                data.name ? data.name :
+                data.original_title ? data.original_title :
+                data.original_name } 
+              </p>
+
+              <Image
+                src={`${imageURL}${data.poster_path}`}
+                alt={`${data.title} Image`}
+                width={220}
+                height={330}
+                className="flex justify-center items-center mb-8 border border-white cursor-pointer"
+              />
+            </div>
+          )
+        })}
       </div>
     </main>
   );
--- src/app/ui/home/search.tsx
+++ src/app/ui/home/search.tsx
@@ -27,10 +27,10 @@ export default function Search({ placeholder }: { placeholder?: string }) {
       <input
         onChange={(e) => handleSearch(e.target.value)}
         placeholder={placeholder}
-        className="peer block w-10/12 text-black rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        className="peer block w-10/12 text-white rounded-md border border-white bg-transparent py-[9px] pl-10 text-sm outline-2 placeholder:text-white"
         defaultValue={searchParams.get('search')?.toString()}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-white peer-focus:text-gray-500" />
     </div>
   )
 }
