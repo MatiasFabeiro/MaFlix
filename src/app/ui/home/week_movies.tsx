bc54a9c5b2c2f2bd68df9a5afe95916601d4ab7f
MatiasFabeiro <matiasfabeiro@gmail.com>
2024-06-12 18:48:47 -0300

    Added some styles

--- src/app/ui/home/week_movies.tsx
+++ src/app/ui/home/week_movies.tsx
@@ -21,22 +21,27 @@ export default async function WeekMovies({ searchQuery }: {searchQuery: string})
         return(
           <div
             key={`${title}-week-${index}`}
-            className="flex flex-col justify-center items-center"
+            className="relative flex flex-col justify-center items-center mb-8 overflow-hidden"
           >
-            <p className="w-11/12 max-w-[200px] h-[80px] flex items-center justify-center text-xl text-center my-4"> 
+            {/* <p className="w-11/12 max-w-[200px] h-[80px] flex items-center justify-center text-xl text-center my-4"> 
               {title} 
-            </p>
+            </p> */}
             
             <Link
               href={`/movie/${movie.id}`}
             >
-              <Image
-                src={`${imageURL}${movie.poster_path}`}
-                alt={`${movie.title} Image`}
-                width={220}
-                height={330}
-                className="flex justify-center items-center mb-8 border border-white cursor-pointer transform transition-all ease-in-out duration-300 hover:scale-110"
-              />
+              <div className="relative w-[220px] h-[330px]">
+                <Image
+                  src={`${imageURL}${movie.poster_path}`}
+                  alt={`${movie.title} Image`}
+                  width={220}
+                  height={330}
+                  className="flex justify-center items-center mb-8 border border-white cursor-pointer transform transition-all ease-in-out duration-300 hover:scale-110"
+                />
+                <div className="absolute bottom-0 w-full bg-black bg-opacity-50 text-white text-center p-2 backdrop-blur-sm">
+                  {title} 
+                </div>
+              </div>
             </Link>
           </div>
         )
